8048cf33265a7a6cebdc26e0f84d3fa7747398df
YSY <getwild@gmail.com>
2024-08-06 19:38:46 +0800

    20240806 增加show、app、index等blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,13 @@
-<div>
-    <h1>The list of tasks</h1>
-</div>
-<div>
-    {{-- @if(count($tasks)) --}}
-    <div>
+@extends('layouts.app')
+
+@section('title','The list of tasks')
+
+@section('content')
        @forelse ($tasks as $task)
        <div>
-        <a href="{{route('tasks.show',['id'=>$task->id])}}">{{$task->title}}</a>
+            <a href="{{route('tasks.show',['id'=>$task->id])}}">{{$task->title}}</a>
        </div>
        @empty
-       <div>
-          There are no Tasks
-       </div>
+        <div>There are no Tasks</div>
        @endforelse
-    </div>
- 
-    {{-- @endif --}}
-</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
 
 class Task
 {
@@ -55,13 +56,23 @@ $tasks = [
     ),
 ];
 
-Route::get('/', function () use ($tasks) {
-    return view('index', ['tasks' => $tasks]);
-})->name('task.index');
+Route::get('/', function () {
+    return redirect()->route('tasks.index');
+});
 
-Route::get('{id}', function ($id) {
-    return 'One single task';
-})->name('tasks.show');
+Route::get('/tasks', function () use ($tasks) {
+
+    return view('index', ['tasks' => $tasks]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id) use ($tasks) {
+
+    $task = collect($tasks)->firstWhere('id', $id);
+    if (!$task) {
+        abort(Response::HTTP_NOT_FOUND);
+    }
+    return view('show', ['task' => $task]);
+})->name('tasks.show',);
 
 //後備路線，當沒有輸入正確網址時導向根目錄
 Route::fallback(function () {
